0a1423254cc54d08f1379c59e3177b4296bc59f9
hossain-shifat <hossainshifat222@gmail.com>
2026-03-07 20:21:00 +0600

    Add menu,logo,cart and banner

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 
-import '@/global.css'
+import "@/global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+    return <Stack screenOptions={{ headerShown: false }} />;
 }
