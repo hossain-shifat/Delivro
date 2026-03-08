2cb4287cdd181ceee5225380b5243656b53ac9aa
hossain-shifat <hossainshifat222@gmail.com>
2026-03-08 19:36:13 +0600

    Fix some error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,16 @@ import { Stack } from "expo-router";
 import "@/global.css";
 import { CartProvider } from "@/context/CartContext";
 import { WishlistProvider } from "@/context/WishlistContext";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
     return (
-        <CartProvider>
-            <WishlistProvider>
-                <Stack screenOptions={{ headerShown: false }} />
-            </WishlistProvider>
-        </CartProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <CartProvider>
+                <WishlistProvider>
+                    <Stack screenOptions={{ headerShown: false }} />
+                </WishlistProvider>
+            </CartProvider>
+        </GestureHandlerRootView>
     );
 }
