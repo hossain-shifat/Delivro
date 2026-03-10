ba0a25e2b19da8d72e48036d12400a42fa27393c
hossain-shifat <hossainshifat222@gmail.com>
2026-03-10 20:10:14 +0600

    Add payment method in checkout page

---
+++ app/checkout.tsx
@@ -0,0 +1,181 @@
+import {
+    View,
+    Text,
+    ActivityIndicator,
+    ScrollView,
+    TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useCart } from "@/context/CartContext";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "@/components/Header";
+import { Address } from "@/constants/types";
+import { dummyAddress } from "@/assets/assets";
+import Toast from "react-native-toast-message";
+import { COLORS } from "@/constants";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Checkout() {
+    const { cartTotal } = useCart();
+    const router = useRouter();
+
+    const [loading, setLoading] = useState(false);
+    const [pageLoading, setPageLoading] = useState(true);
+
+    const [selectedAddress, setSelectedAddress] = useState<Address | null>(
+        null,
+    );
+    const [paymentMethod, setPaymentMethod] = useState<"cash" | "stripe">(
+        "cash",
+    );
+
+    const shipping = 2.0;
+    const tax = 0;
+    const total = cartTotal + shipping + tax;
+
+    const fetchAddress = async () => {
+        const addrList = dummyAddress;
+        if (addrList.length > 0) {
+            // find defauld address
+            const def = addrList.find((a: any) => a.isDefault) || addrList[0];
+            setSelectedAddress(def as Address);
+        }
+        setPageLoading(false);
+    };
+
+    const handlePlaceOrder = async () => {
+        if (!selectedAddress) {
+            Toast.show({
+                type: "error",
+                text1: "Error",
+                text2: "Please add a shipping address",
+            });
+            return;
+        }
+        if (paymentMethod === "stripe") {
+            return Toast.show({
+                type: "error",
+                text1: "Info",
+                text2: "Stripe not implimented yet",
+            });
+        }
+        // cash on delivery
+        router.replace("/order");
+    };
+
+    useEffect(() => {
+        fetchAddress();
+    }, []);
+
+    if (pageLoading) {
+        return (
+            <SafeAreaView className="flex-1 bg-surface justify-center items-center">
+                <ActivityIndicator size="large" color={COLORS.primary} />
+            </SafeAreaView>
+        );
+    }
+
+    return (
+        <SafeAreaView className="flex-1 bg-surface" edges={["top"]}>
+            <Header title="Checkout" showBack />
+            <ScrollView className="flex-1 px-4 mt-4">
+                {/* Address section */}
+                <Text className="text-lg font-bold text-primary mb-4">
+                    Shipping Address
+                </Text>
+                {selectedAddress ? (
+                    <View className="bg-white p-4 rounded-xl mb-6 shadow-sm">
+                        <View className="flex-row items-start justify-between mb-2">
+                            <Text className="text-base font-bold">
+                                {selectedAddress.type}
+                            </Text>
+                            <TouchableOpacity
+                                className=""
+                                onPress={() => router.push("/addresses")}
+                            >
+                                <Text className="text-accent text-sm">
+                                    Change
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <Text className="text-secondary leading-5">
+                            {selectedAddress.street}, {selectedAddress.city}
+                            {"\n"}
+                            {selectedAddress.state},{selectedAddress.zipCode}
+                            {"\n"}
+                            {selectedAddress.country}
+                        </Text>
+                    </View>
+                ) : (
+                    <TouchableOpacity
+                        className="bg-white p-6 rounded-xl mb-6 items-center justify-center border-dashed border-2 border-gray-100"
+                        onPress={() => router.push("/addresses")}
+                    >
+                        <Text className="text-primary font-bold">
+                            Add Address
+                        </Text>
+                    </TouchableOpacity>
+                )}
+                {/* payment section */}
+                <Text className="text-lg font-bold text-primary mb-4">
+                    Payment Method
+                </Text>
+                {/* cash on delivery option */}
+                <TouchableOpacity
+                    className={`flex-row bg-white p-4 rounded-xl mb-4 shadow-sm items-center border-2 ${paymentMethod === "cash" ? "border-primary" : "border-transparent"}`}
+                    onPress={() => setPaymentMethod("cash")}
+                >
+                    <Ionicons
+                        name="cash-outline"
+                        size={24}
+                        color={COLORS.primary}
+                        className="mr-3 font-bold text-primary mb-4"
+                    />
+                    <View className="ml-3 flex-1">
+                        <Text className="text-base font-bold text-primary">
+                            Cash on delivery
+                        </Text>
+                        <Text className="text-secondary text-xs mt-1">
+                            Pay when you receive the order
+                        </Text>
+                    </View>
+                    {paymentMethod === "cash" && (
+                        <Ionicons
+                            name="checkmark-circle"
+                            size={24}
+                            color={COLORS.primary}
+                        />
+                    )}
+                </TouchableOpacity>
+                {/* Stripe option */}
+                <TouchableOpacity
+                    className={`flex-row bg-white p-4 rounded-xl mb-4 shadow-sm items-center border-2 ${paymentMethod === "stripe" ? "border-primary" : "border-transparent"}`}
+                    onPress={() => setPaymentMethod("stripe")}
+                >
+                    <Ionicons
+                        name="card-outline"
+                        size={24}
+                        color={COLORS.primary}
+                        className="mr-3 font-bold text-primary mb-4"
+                    />
+                    <View className="ml-3 flex-1">
+                        <Text className="text-base font-bold text-primary">
+                            Pay with Card
+                        </Text>
+                        <Text className="text-secondary text-xs mt-1">
+                            Cradit or Devid Card
+                        </Text>
+                    </View>
+                    {paymentMethod === "stripe" && (
+                        <Ionicons
+                            name="checkmark-circle"
+                            size={24}
+                            color={COLORS.primary}
+                        />
+                    )}
+                </TouchableOpacity>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
