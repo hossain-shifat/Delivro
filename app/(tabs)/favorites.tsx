69cd653ef8f44561952a94eccd60885f2eb135d0
hossain-shifat <hossainshifat222@gmail.com>
2026-03-10 09:52:25 +0600

    Add wishlist page

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import React from "react";
+import { useWishlist } from "@/context/WishlistContext";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "@/components/Header";
+import ProductCard from "@/components/ProductCard";
 
-export default function Favorite() {
-  return (
-    <View>
-      <Text>Favorite</Text>
-    </View>
-  )
+export default function Favorites() {
+    const { wishlist } = useWishlist();
+    const router = useRouter();
+
+    return (
+        <SafeAreaView className="flex-1 bg-surface" edges={["top"]}>
+            <Header title="Wishlist" showMenu showCart />
+            {wishlist.length ? (
+                <ScrollView
+                    className="flex-1 px-4 mt-4"
+                    showsVerticalScrollIndicator={false}
+                >
+                    <View className="flex-row flex-wrap justify-between">
+                        {wishlist.map((product) => (
+                            <ProductCard key={product._id} product={product} />
+                        ))}
+                    </View>
+                </ScrollView>
+            ) : (
+                <View className="flex-1 items-center justify-center">
+                    <Text className="text-secondary text-lg">
+                        Your wishlist is empty
+                    </Text>
+                    <TouchableOpacity
+                        className="mt-4"
+                        onPress={() => router.push("/")}
+                    >
+                        <Text className="text-primary font-bold">
+                            Start Shopping
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+        </SafeAreaView>
+    );
 }
